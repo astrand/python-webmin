4367a05fdfd159c95b74c5f273fac89892e8a405
Peter Åstrand <astrand@lysator.liu.se>
2002-06-04 11:37:35 +0000

    Added license header.

--- webmin.py
+++ webmin.py
@@ -1,5 +1,23 @@
+#!/usr/bin/env python2
+#
 # webmin.py
 # Python implementation of web-lib.pl
+#
+# Written by Peter Åstrand <peter@cendio.se>
+# Copyright (C) 2002 Cendio Systems AB (http://www.cendio.se)
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License. 
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #
 # Coding style: Max linewidth 120 chars
