86bbda84ecb6324b7d79aa215a6750774c335ae1
Peter Åstrand <astrand@lysator.liu.se>
2002-06-29 17:29:56 +0000

    removed cp2webmindir.sh

--- cp2webmindir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cp -a examples/uptimepl/* /usr/libexec/webmin/uptimepl/
-
-rm -f examples/uptimepy/*.pyc
-rm -f examples/uptimepy/*.pyo
-cp -a examples/uptimepy/* /usr/libexec/webmin/uptimepy/
-
