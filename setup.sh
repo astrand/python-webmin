a319a5970d8bf4a5a8aa7c5ba8d36b6a0022bd12
Peter Åstrand <astrand@lysator.liu.se>
2004-07-01 09:40:57 +0000

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+
+PATH=$PATH:/usr/local/bin
+for binary in python2 python2.0 python2.1 python2.2 python2.3; do
+    if $binary -c "" ; then
+        $binary - <<EOF
+import sys
+sitedir = sys.prefix + "/lib/python" + sys.version[:3] + "/site-packages"
+filename = sitedir + "/python-webmin.pth"
+open(filename, "w").write("/opt/python-webmin\n")
+print "Created", filename
+EOF
+    fi
+done
