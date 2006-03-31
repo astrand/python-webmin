3398404d74df944a76da9ebc55a10ea41ae5f776
Peter Åstrand <astrand@lysator.liu.se>
2006-03-31 12:47:44 +0000

    Correctly find site-packages on systems with /lib64

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
+#!/bin/sh
 
 PATH=$PATH:/usr/local/bin
 for binary in python python2 python2.0 python2.1 python2.2 python2.3 python2.4 python2.5; do
     if $binary -c "" ; then
         $binary - <<EOF
 import sys
-sitedir = sys.prefix + "/lib/python" + sys.version[:3] + "/site-packages"
-filename = sitedir + "/python-webmin.pth"
+sitedirs = filter(lambda s: s.endswith("site-packages"), sys.path)
+if len(sitedirs) < 1:
+    sys.exit("Unable to find a site-packages directory in sys.path")
+filename = sitedirs[0] + "/python-webmin.pth"
 open(filename, "w").write("/opt/python-webmin\n")
 print "Created", filename
 EOF
