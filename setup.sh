ff77efda2012ecd32a2ef4534cb996d24712e1b9
Peter Åstrand <astrand@lysator.liu.se>
2006-05-18 10:49:31 +0000

    Do not use string.endswith(), since it's not available in Python 1.5.

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ for binary in python2 python2.0 python2.1 python2.2 python2.3 python2.4 python2.
         last_working=${binary}
         ${binary} - <<EOF
 import sys
-sitedirs = filter(lambda s: s.endswith("site-packages"), sys.path)
+sitedirs = filter(lambda s: s[-len("site-packages"):] == "site-packages", sys.path)
 if len(sitedirs) < 1:
     sys.exit("Unable to find a site-packages directory in sys.path")
 filename = sitedirs[0] + "/python-webmin.pth"
