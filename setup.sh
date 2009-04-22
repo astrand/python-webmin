d53594fed9857cd7cd4c5a404c1f5155084f498b
Peter Åstrand <astrand@lysator.liu.se>
2009-04-22 12:20:18 +0000

    This patch is just like the Hiveconf patchset 196:
    Find the directory to create the .pth file in by using distutils,
    instead of digging in sys.path. This should make the setup script work
    on (stupid!) Ubuntu systems where site-packagages has been renamed to
    dist-packages.
    
    I've also removed python2.0 and python2.1 from the list; let's require
    Python 2.2 or newer.

--- setup.sh
+++ setup.sh
@@ -3,15 +3,12 @@
 last_working=""
 
 # Create .pth files
-for binary in python2 python2.0 python2.1 python2.2 python2.3 python2.4 python2.5 python _python_preferred_; do
+for binary in python2 python2.2 python2.3 python2.4 python2.5 python2.6 python _python_preferred_; do
     if ${binary} -c "" 2>/dev/null; then
         last_working=${binary}
         ${binary} - <<EOF
-import sys
-sitedirs = filter(lambda s: s[-len("site-packages"):] == "site-packages", sys.path)
-if len(sitedirs) < 1:
-    sys.exit("Unable to find a site-packages directory in sys.path")
-filename = sitedirs[0] + "/python-webmin.pth"
+from distutils import sysconfig
+filename = sysconfig.get_python_lib() + "/python-webmin.pth"
 open(filename, "w").write("/opt/python-webmin\n")
 print "Created", filename
 EOF
