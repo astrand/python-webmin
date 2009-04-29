59872e7c0f9bec998a0a51dd2f9bc304c7267a8a
Peter Åstrand <astrand@lysator.liu.se>
2009-04-29 13:56:01 +0000

    Separate configuration from logic.

--- setup.sh
+++ setup.sh
@@ -7,9 +7,11 @@ for binary in python2 python2.2 python2.3 python2.4 python2.5 python2.6 python _
     if ${binary} -c "" 2>/dev/null; then
         last_working=${binary}
         ${binary} - <<EOF
+pthfile="python-webmin.pth"
+mod_dir="/opt/python-webmin"
 from distutils import sysconfig
-filename = sysconfig.get_python_lib() + "/python-webmin.pth"
-open(filename, "w").write("/opt/python-webmin\n")
+filename = sysconfig.get_python_lib() + "/" + pthfile
+open(filename, "w").write(mod_dir + "\n")
 print "Created", filename
 EOF
     fi
